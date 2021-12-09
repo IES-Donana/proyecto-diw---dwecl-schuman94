b184587d37a2f42ef0f6db022a393f930f1a3c93
sergio <sergio.chulian@iesdonana.org>
2021-12-09 21:12:59 +0100

    empezando js

--- script.js
+++ script.js
@@ -6,3 +6,49 @@
  y en otro el blur
 
  */
+
+let formulario = document.getElementById("formulario-contacto");
+let enviar = document.getElementById("enviar");
+
+enviar.addEventListener('click', validar);
+
+function error (elemento) {
+    document.getElementById("mensaje-error").innerHTML = elemento.validationMessage;
+    elemento.className = "error";
+}
+
+function borrarError(elemento) {
+    for (let e of formulario) {
+        e.setCustomValidity("");
+        document.getElementById("mensaje-error").innerHTML = e.validationMessage;
+    }
+} 
+
+function validar(e) {
+    if (validaNombre() && confirm("¿Enviar formulario?")) {
+        e.preventDefault();
+        return true;
+    } else {
+        e.preventDefault();
+        return false;
+    }
+}
+
+function validaNombre() {
+    borrarError();
+    let elemento = document.querySelector("#form-nombre");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            elemento.setCustomValidity("Debes introducir un nombre");
+        }
+        if (elemento.validity.tooLong) {
+            elemento.setCustomValidity("El nombre no debe superar " + elemento.maxLength + " caracteres");
+        }
+        elemento.reportValidity();
+        error(elemento);
+        return false;        
+    }
+    console.log("Nombre valido");
+    return true;
+}
+
